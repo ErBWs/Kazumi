8f2e300c822ea35d8561cede84c2d9b2196b7315
玲羽 <195327324+UzawaReisaQwQ@users.noreply.github.com>
2026-06-19 19:08:23 +0800

    feat: add collect page count (#1783)
    * feat: add collect page anime count
    
    * fix: collect page
    
    * feat: add collect page counter
    
    * feat: add showAnimeCounter settings
    
    (cherry picked from commit 8fcae5581714dd04c48a1ad9d30641be77192dda)

--- lib/services/storage/settings_keys.dart
+++ lib/services/storage/settings_keys.dart
@@ -426,6 +426,11 @@ class SettingsKeys {
     true,
     group: SettingGroup.interface,
   );
+  static const showAnimeCounter = SettingKey<bool>(
+    _SettingBoxKey.showAnimeCounter,
+    false,
+    group: SettingGroup.interface,
+  );
   static const downloadParallelEpisodes = SettingKey<int>(
     _SettingBoxKey.downloadParallelEpisodes,
     2,
@@ -570,6 +575,7 @@ class SettingsKeys {
     proxyUrl,
     proxyTestUrl,
     showRating,
+    showAnimeCounter,
     downloadParallelEpisodes,
     downloadParallelSegments,
     downloadDanmaku,
@@ -678,6 +684,7 @@ class _SettingBoxKey {
       proxyUrl = 'proxyUrl',
       proxyTestUrl = 'proxyTestUrl',
       showRating = 'showRating',
+      showAnimeCounter = 'showAnimeCounter',
       downloadParallelEpisodes = 'downloadParallelEpisodes',
       downloadParallelSegments = 'downloadParallelSegments',
       downloadDanmaku = 'downloadDanmaku',
